01f1ea680644621f65deac32456715a65d8b9f7d
bilinkis <nico@bilinkis.com>
2024-05-16 17:19:17 -0300

    autodiag en la navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import { Link as ScrollLink } from 'react-scroll';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router'; 
+
 
 const Header = () => {
+    const router = useRouter();
     return (
         <header className="bg-gradient-to-r from-blue-500 to-indigo-800 text-white py-6 sticky top-0 z-50 shadow-lg">
             <div className="container mx-auto flex justify-between items-center">
@@ -14,6 +17,13 @@ const Header = () => {
                 </Link>
                 <nav>
                     <ul className="flex space-x-6">
+                        {router.pathname !== '/diagnostico' && (
+                        <li className="relative group">
+                            <Link href="/diagnostico" className="cursor-pointer">
+                                <span className="transition-transform transform hover:scale-110 group-hover:shadow-lg group-hover:bg-white group-hover:text-blue-500 rounded-md px-2 py-1">Autodiagnóstico</span>
+                            </Link>
+                        </li>
+                        )}
                         <li className="relative group">
                             <ScrollLink to="description" smooth={true} offset={-72} className="cursor-pointer">
                                 <span className="transition-transform transform hover:scale-110 group-hover:shadow-lg group-hover:bg-white group-hover:text-blue-500 rounded-md px-2 py-1">Sobre Nosotros</span>
