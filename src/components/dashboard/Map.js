7e7f4ffc4f6c9e556099d1a3a2acf757f029321d
bilinkis <nico@bilinkis.com>
2024-06-27 12:03:05 -0300

    corrections to the map

--- src/components/dashboard/Map.js
+++ src/components/dashboard/Map.js
@@ -21,7 +21,7 @@ const Map = ({ steps, toggleStepCompletion, onSubStepClick }) => {
                 <rect width={totalWidth} height="410" fill="#bdc3c7" />
 
                 {/* Línea que conecta los pasos */}
-                <path d={`M150 200 L${totalWidth - 150} 200`} stroke="#00aaff" strokeWidth="4" fill="transparent" strokeDasharray="10 10" />
+                <path d={`M150 100 L${totalWidth - 150} 100`} stroke="#2d3436" strokeWidth="4" fill="transparent" strokeDasharray="10 10" />
 
                 {/* Iconos y pasos */}
                 {steps.map((step, index) => {
@@ -29,12 +29,12 @@ const Map = ({ steps, toggleStepCompletion, onSubStepClick }) => {
                     const strokeDasharray = 2 * Math.PI * 40; // Circunferencia del círculo
                     const strokeDashoffset = strokeDasharray - (strokeDasharray * (percentage / 100));
                     return (
-                        <g key={step.id} transform={`translate(${150 + index * 200}, 200)`}>
+                        <g key={step.id} transform={`translate(${150 + index * 200}, 100)`}>
                             <circle
                                 cx="0"
                                 cy="0"
                                 r="40"
-                                fill="#95a5a6"
+                                fill="#ffffff"
                                 stroke={step.completed ? '#2ecc71' : '#e74c3c'}
                                 strokeWidth="3"
                                 onClick={() => handleExpand(step.id)}
@@ -52,9 +52,9 @@ const Map = ({ steps, toggleStepCompletion, onSubStepClick }) => {
                                 transform="rotate(-90) translate(0, 0)"
                                 className="cursor-pointer"
                             />
-                            <foreignObject x="-20" y="-20" width="40" height="40">
-                                <div className="flex items-center justify-center w-full h-full">
-                                    <i className={`fas ${step.icon} text-white text-2xl`}></i>
+                            <foreignObject x="-20" y="-20" width="40" height="40" onClick={() => handleExpand(step.id)}>
+                                <div className="flex items-center justify-center w-full h-full cursor-pointer">
+                                    <i className={`fas ${step.icon} text-[#2d3436] text-2xl`}></i>
                                 </div>
                             </foreignObject>
                             <text
@@ -68,20 +68,20 @@ const Map = ({ steps, toggleStepCompletion, onSubStepClick }) => {
                                 {step.step}
                             </text>
                             {expandedStep === step.id && (
-                                <g transform="translate(0, 80)">
+                                <g transform="translate(-60, 100)">
                                     {step.subSteps.map((subStep, subIndex) => (
                                         <g
                                             key={subStep.id}
-                                            transform={`translate(0, ${subIndex * 40})`}
+                                            transform={`translate(0, ${subIndex * 50})`}
                                             onClick={() => onSubStepClick({ ...subStep, parentId: step.id })}
-                                            className="cursor-pointer"
+                                            className="cursor-pointer align-left"
                                         >
                                             <circle
                                                 cx="0"
                                                 cy="0"
-                                                r="15"
+                                                r="10"
                                                 fill={subStep.completed ? '#2ecc71' : '#e74c3c'}
-                                                stroke="white"
+                                                stroke="#2d3436"
                                                 strokeWidth="2"
                                             />
                                             <text
