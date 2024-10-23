629847407c758b945702b1a1186c9347b9269695
Nico Bilinkis <nico@bilinkis.com>
2024-10-23 12:36:35 -0300

    boton mas pequeño

--- src/components/Buy.js
+++ src/components/Buy.js
@@ -1,9 +1,7 @@
-import { redirect } from "next/dist/server/api-utils";
-
 const Buy = ({ setPaid }) => {
   return (
     <button
-      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4"
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded mt-4 w-1/2 mx-auto"
       onClick={() => {
         if (setPaid !== undefined) {
           setPaid(true);
