179e155f91c318191eebfdd02fe1294a53295cfc
Nico Bilinkis <nico@bilinkis.com>
2024-10-23 12:31:57 -0300

    sticky footer para autodiag

--- pages/diagnostico.js
+++ pages/diagnostico.js
@@ -1,17 +1,17 @@
-import Head from 'src/components/Head';
+import Head from "src/components/Head";
 import Header from "../src/components/Header";
 import Footer from "../src/components/Footer";
 import Form from "../src/components/Form";
 
 export default function Diagnostico() {
-    return (
-        <div>
-            <Head />
-            <Header />
-            <main className='grow'>
-                <Form />
-            </main>
-            <Footer />
-        </div>
-    )
+  return (
+    <div className="min-h-screen">
+      <Head />
+      <Header />
+      <main className="flex-grow">
+        <Form />
+      </main>
+      <Footer />
+    </div>
+  );
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,18 @@
+import { useRouter } from "next/router";
 const Footer = () => {
-    return (
-        <footer className="bg-gray-800 text-white py-6 w-screen">
-            <div className="container mx-auto text-center">
-                <p>&copy; 2024 Emigrapp. Todos los derechos reservados.</p>
-            </div>
-        </footer>
-    );
+  const router = useRouter();
+  const route = router.pathname;
+  return (
+    <footer
+      className={`bg-gray-800 text-white py-6 w-screen ${
+        route === "/diagnostico" && "fixed bottom-0"
+      }`}
+    >
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Emigrapp. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
